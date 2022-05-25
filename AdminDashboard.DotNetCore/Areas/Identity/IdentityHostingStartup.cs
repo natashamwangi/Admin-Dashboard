c5f40a32513578348cf20773daa3713c8a1c5b34
Nancy Mwangi <natashamwangi1@gmail.com>
2022-05-25 22:37:37 +0300

    ASP.NET dashboard use to track sales

---
+++ AdminDashboard.DotNetCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(AdminDashboard.DotNetCore.Areas.Identity.IdentityHostingStartup))]
+namespace AdminDashboard.DotNetCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ AdminDashboard.DotNetCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AdminDashboard.DotNetCore.Models.Identity;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard.DotNetCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IDataProtectionKeyContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+    }
+}
